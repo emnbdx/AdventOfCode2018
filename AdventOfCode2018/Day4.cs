0d292cf0e8cff4911e833500cf7773d3cbbc4674
Eddy <eddy.montus@gmail.com>
2018-12-11 00:56:48 +0100

    continue day 4

--- AdventOfCode2018/Day4.cs
+++ AdventOfCode2018/Day4.cs
@@ -1,19 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
 namespace AdventOfCode2018
 {
     public class Day4 : AbstractDay
     {
+        private Dictionary<DateTime, string> comportements = new Dictionary<DateTime, string>();
+
         public Day4() : base(4)
-        { }
+        {
+            foreach(var line in data)
+            {
+                var parts = line.Split(']');
+                comportements.Add(DateTime.ParseExact(parts[0].Replace("[", ""), "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture), parts[1].Trim());
+            }
+        }
 
         public override string Part1()
         {
-            throw new NotImplementedException();
+            var guards = new Dictionary<int, Guard>();
+            foreach(var comportement in comportements.OrderBy(_ => _.Key))
+            {
+                if (comportement.Value.StartsWith("Guard"))
+                {
+                    var id = int.Parse(comportement.Value.Split(' ')[1].Replace("#", ""));
+
+                    if (!guards.ContainsKey(id))
+                    {
+                        guards.Add(id, new Guard(id));
+                    }
+                }
+            }
+
+            return null;
         }
 
         public override string Part2()
         {
             throw new NotImplementedException();
+        }
+    }
+
+    public class Guard
+    {
+        public int Id { get; }
+        public int SleepTime { get; private set; }
+
+        public Guard(int id)
+        {
+            Id = id;
+            SleepTime = 0;
+        }
+
+        public void AddSleepTime(int time)
+        {
+            SleepTime += time;
         }
     }
 }
--- AdventOfCode2018/Program.cs
+++ AdventOfCode2018/Program.cs
@@ -7,6 +7,7 @@
             new Day1().Compute();
             new Day2().Compute();
             new Day3().Compute();
+            new Day4().Compute();
         }
     }
 }
